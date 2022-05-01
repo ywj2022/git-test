7c33edb366c3deed18e2fba0ee6c4e06864201bd
yangwenjie <yangwenjietju@126.com>
2022-05-01 23:15:15 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author yangwenjie
+ * @create 2022-05-01 23:12
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
